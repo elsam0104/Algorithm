49c52fde98fa8c891dc691fb0fbc58c2d8edddaf
YeEun Oh <OhSolDev@gmail.com>
2022-11-23 23:51:56 +0900

    1123 정렬 완성

--- Algorithm_Sort/pracice_quick.cpp
+++ Algorithm_Sort/pracice_quick.cpp
@@ -1,47 +1,48 @@
-//#include<iostream>
-//using namespace std;
-//int a[100];
-//void quickSort(int start, int end) {
-//	if (start >= end) {	//원소가 1개인 경우 
-//		return;
-//	}
-//	int key = start;	//키는 첫번째 원소
-//	int left = start + 1;	//왼쪽 출발 지점 
-//	int right = end;		//오른쪽 출발 지점 
-//	int temp;
-//
-//	while (left < right) {	//엇갈릴 때까지 반복 
-//		while (//) {	//키 값보다 큰 값을 만날 때까지 
-//			left++;
-//		}
-//		while (//) {	//키 값보다 작은 값을 만날 때까지 
-//			right--;
-//		}
-//		if (left >= right) {	//현재 엇갈린 상태면 키 값과 교체
-//			temp = a[right];
-//			a[right] = a[key];
-//			a[key] = temp;
-//		}
-//		else {		//엇갈리지 않았다면 i와 j를 교체
-//			temp = a[left];
-//			a[left] = a[right];
-//			a[right] = temp;
-//		}
-//	}
-//	quickSort(start, right - 1);
-//	quickSort(right + 1, end);
-//}
-//
-//int main() {
-//	int n, i;
-//	cin >> n;
-//	for (i = 0; i < n; i++) {
-//		cin >> a[i];
-//	}
-//	quickSort(0, n - 1);
-//	for (i = 0; i < n; i++) {
-//		cout << a[i] << " ";
-//	}
-//	return 0;
-//}
-//
+#include<iostream>
+using namespace std;
+int a[100];
+void quickSort(int start, int end) {
+	if (start >= end) {	//원소가 1개인 경우 
+		return;
+	}
+	int key = start;	//키는 첫번째 원소
+	int left = start + 1;	//왼쪽 출발 지점 
+	int right = end;		//오른쪽 출발 지점 
+	int temp;
+
+	while (left < right) {	//엇갈릴 때까지 반복 
+		while (a[left] < a[key]) {	//키 값보다 큰 값을 만날 때까지 
+			left++;
+		}
+		while (a[right] > a[key]) {	//키 값보다 작은 값을 만날 때까지 
+			right--;
+		}
+		if (left >= right) {	//현재 엇갈린 상태면 키 값과 교체
+			temp = a[right];
+			a[right] = a[key];
+			a[key] = temp;
+		}
+		else {		//엇갈리지 않았다면 i와 j를 교체
+			temp = a[left];
+			a[left] = a[right];
+			a[right] = temp;
+		}
+	}
+	quickSort(start, right - 1);
+	quickSort(right + 1, end);
+}
+
+int main() {
+	int n, i;
+	cin >> n;
+	for (i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	quickSort(0, n - 1);
+	for (i = 0; i < n; i++) {
+		cout << a[i] << " ";
+	}
+	return 0;
+}
+
+
--- Algorithm_Sort/practice_merge.cpp
+++ Algorithm_Sort/practice_merge.cpp
@@ -1,45 +1,45 @@
-//#include<iostream>
-//using namespace std;
-//
-//int a[101], sorted[101];
-//
-//void merge(int left, int right) {
-//	int mid;
-//	int p1, p2, p3;
-//
-//	if (left < right) {
-//		mid = (left + right) / 2;
-//		merge(left, mid);
-//		merge(mid + 1, right);
-//		p1 = left; 
-//		p2 = mid + 1; 
-//		p3 = left; 
-//
-//		while (p1 <= mid && p2 <= right) {
-//			if (//)sorted[p3++] = a[p1++];
-//			else sorted[p3++] = a[p2++];
-//		}
-//
-//		while (p1 <= mid) sorted[p3++] = a[p1++];
-//
-//		while (p2 <= right) sorted[p3++] = a[p2++];
-//		
-//		배열 sorted를 배열 a로 재복사
-//		for (int i = left; i <= right; i++) {
-//			a[i] = sorted[i];
-//		}
-//	}
-//}
-//
-//int main() {
-//	int n, i;
-//	cin >> n; 
-//	for (i = 1; i <= n; i++) {
-//		cin >> a[i];
-//	}
-//	merge(1, n);
-//	for (i = 1; i <= n; i++) {
-//		cout << a[i] << " ";
-//	}
-//	return 0;
-//}
+#include<iostream>
+using namespace std;
+
+int a[101], sorted[101];
+
+void merge(int left, int right) {
+	int mid;
+	int p1, p2, p3;
+
+	if (left < right) {
+		mid = (left + right) / 2;
+		merge(left, mid);
+		merge(mid + 1, right);
+		p1 = left; 
+		p2 = mid + 1; 
+		p3 = left; 
+
+		while (p1 <= mid && p2 <= right) {
+			if (a[p1] < a[p2])sorted[p3++] = a[p1++];
+			else sorted[p3++] = a[p2++];
+		}
+
+		while (p1 <= mid) sorted[p3++] = a[p1++];
+
+		while (p2 <= right) sorted[p3++] = a[p2++];
+		
+		//배열 sorted를 배열 a로 재복사
+		for (int i = left; i <= right; i++) {
+			a[i] = sorted[i];
+		}
+	}
+}
+
+int main() {
+	int n, i;
+	cin >> n; 
+	for (i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+	merge(1, n);
+	for (i = 1; i <= n; i++) {
+		cout << a[i] << " ";
+	}
+	return 0;
+}
