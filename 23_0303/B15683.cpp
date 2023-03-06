5f7b2fde91a433de6ff9ec594dc9d61db68a14d1
YeEun Oh <OhSolDev@gmail.com>
2023-03-06 20:28:40 +0900

    게임 대기시 인원 파악 ui 제작

---
+++ 23_0303/B15683.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int arr[4][2] = { {0,1},{1,0},{0,-1},{-1,0} };
+int DPS(int type, int x, int y)
+{
+	switch (type)
+	{
+	case 1:
+
+		break;
+	case 2:
+
+		break;
+	case 3:
+
+		break;
+	case 4:
+
+		break;
+	case 5:
+
+		break;
+	default:
+		break;
+	}
+}
+int main()
+{
+
+}
